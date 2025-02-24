dccac8d11d59bb290cba37beb4428b85961f1043
refrain62 <nao_h13@hotmail.com>
2025-02-24 23:00:57 +0900

    GitHubでのログインまで

--- src/api/auth/[...nextauth]/route.ts
+++ src/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,5 @@
 import { handles } from "@/auth"
 
 export const { GET, POST } = handles
+
+export default handles
--- src/auth.ts
+++ src/auth.ts
@@ -1,9 +1,9 @@
-import NextAuth from "next-auth";
-import Github
-impot { prisma } from "./lib/github";
-import { PrismaAdapter } from "@auth/prisma-adapter"; 
-
+import NextAuth from "next-auth"
+import GitHub from "next-auth/providers/github"
+import { prisma } from "./lib/prisma"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+ 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
-  providers: [Github],
+  providers: [GitHub],
 })
